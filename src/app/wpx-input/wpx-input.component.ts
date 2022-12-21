f133b6a59f76785445718c4ab57232b76e325bd1
akbardoosti <83670691+akbardoosti@users.noreply.github.com>
2022-12-21 10:10:31 +0330

    3 files modified

--- src/app/wpx-input/wpx-input.component.ts
+++ src/app/wpx-input/wpx-input.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-wpx-input',
   templateUrl: './wpx-input.component.html',
-  styleUrls: ['./wpx-input.component.css']
+  styleUrls: ['./wpx-input.component.css'],
 })
 export class WpxInputComponent implements OnInit {
+  @Input() cType: any = 'text';
+  @Input() cId: any = 'text';
+  @Input() cClass: any = 'text';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
